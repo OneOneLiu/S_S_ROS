eeccecaa3e6fe25a8ba1ccd6035c09195cbda93a
1080 Desktop <daohui.liu@mail.utoronto.ca>
2024-05-03 18:44:23 -0400

    fix replaced src folder

--- Docker/noetic.bash
+++ Docker/noetic.bash
@@ -5,7 +5,10 @@ docker run --rm -it \
     --env="DISPLAY=$DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --env="XAUTHORITY=$XAUTH" \
-    --volume="/home/$USER/catkin_ws/src/s_s_ros/src:/catkin_ws/src" \
+    --volume="/home/$USER/catkin_ws/src/s_s_ros/src/pkg_camera:/catkin_ws/src/pkg_camera" \
+    --volume="/home/$USER/catkin_ws/src/s_s_ros/src/pkg_robot:/catkin_ws/src/pkg_robot" \
+    --volume="/home/$USER/catkin_ws/src/s_s_ros/src/pkg_gripper:/catkin_ws/src/pkg_gripper" \
+    --volume="/home/$USER/catkin_ws/src/s_s_ros/src/pkg_grasp_main:/catkin_ws/src/pkg_grasp_main" \
     --volume="/dev/bus/usb:/dev/bus/usb" \
     --volume="/tmp/.docker.xauth:/tmp/.docker.xauth:rw" \
     --net=host \
