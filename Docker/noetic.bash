b228f1b370d67e2c07b4d9d2f0e1c1cae7e500ed
1080 Desktop <daohui.liu@mail.utoronto.ca>
2024-05-06 22:34:50 -0400

    Add support for pkg_robot_gripper

--- Docker/noetic.bash
+++ Docker/noetic.bash
@@ -16,6 +16,7 @@ docker run --rm -it \
     --volume="/home/$USER/catkin_ws/src/s_s_ros/src/pkg_gripper:/catkin_ws/src/pkg_gripper" \
     --volume="/home/$USER/catkin_ws/src/s_s_ros/src/pkg_grasp_main:/catkin_ws/src/pkg_grasp_main" \
     --volume="/home/$USER/catkin_ws/src/s_s_ros/src/pkg_urdf:/catkin_ws/src/pkg_urdf" \
+    --volume="/home/$USER/catkin_ws/src/s_s_ros/src/pkg_robot_gripper:/catkin_ws/src/pkg_robot_gripper" \
     --volume="/dev/bus/usb:/dev/bus/usb" \
     --volume="/tmp/.docker.xauth:/tmp/.docker.xauth:rw" \
     --net=host \
